30816808ac6f95cabad63b2451303c92cb5f59ac
Ehsan Asdar <ehsanmasdar@gmail.com>
2016-06-19 22:49:21 -0500

    Don't commit that

--- LASA Schedules/src/main/java/com/asdar/lasaschedules/service/SchedulesFirebaseMessagingService.java
+++ LASA Schedules/src/main/java/com/asdar/lasaschedules/service/SchedulesFirebaseMessagingService.java
@@ -1,5 +1,7 @@
 package com.asdar.lasaschedules.service;
 
+import android.util.Log;
+
 import com.asdar.lasaschedules.util.Resources;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -10,6 +12,7 @@ public class SchedulesFirebaseMessagingService extends FirebaseMessagingService 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         try {
+            Log.d("firebase","Got firebase message");
             Resources.updateSchedules(getApplicationContext());
         } catch (IOException e) {
             e.printStackTrace();
